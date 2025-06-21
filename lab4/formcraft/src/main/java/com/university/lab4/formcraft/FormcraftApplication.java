d558de869f3b204ee99230cd07c75ed29d5b0234
zhuravlovO <zhuravlov.ov@gmail.com>
2025-06-21 21:05:19 +0300

    Основа Лаби 4

---
+++ lab4/formcraft/src/main/java/com/university/lab4/formcraft/FormcraftApplication.java
@@ -0,0 +1,13 @@
+package com.university.lab4.formcraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormcraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormcraftApplication.class, args);
+	}
+
+}
---
+++ lab4/formcraft/src/test/java/com/university/lab4/formcraft/FormcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.lab4.formcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
